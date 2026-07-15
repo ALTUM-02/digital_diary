3db30ea51ecc34759d30f51b58340c7841a74fea
ALTUM-02 <martin249jesus@gmail.com>
2026-07-15 13:01:32 +0300

    new chage

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -19,7 +19,11 @@ from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
+from django.conf import settings
+from django.conf.urls.static import static
+from api.views import JWTGraphQLView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("graphql/", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
