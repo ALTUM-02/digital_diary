4e71ffdc56daac599a69a9cf74ef86df4510695e
ALTUM-02 <martin249jesus@gmail.com>
2026-07-15 16:06:34 +0300

    new chage

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
-                "django.template.context_processors.media",
+                "django.template.context_processors.messages",
             ],
         },
     },
