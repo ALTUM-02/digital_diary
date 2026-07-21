7396c76cb3fb3cc80dffb8f39205f69420ced6cc
ALTUM-02 <martin249jesus@gmail.com>
2026-07-22 00:05:16 +0300

    new chanag

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { Toaster as Sonner } from "@/components/ui/sonner";
+
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
@@ -34,7 +34,7 @@ const App = () => (
           <DiaryProvider>
             <TooltipProvider>
               <Toaster />
-              <Sonner />
+              
               <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
                 <Routes>
                   <Route path="/login" element={<LoginPage />} />
