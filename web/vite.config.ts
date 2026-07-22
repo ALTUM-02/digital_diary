5e6563d4ffb6afc170d38c91ad6f8f3500729a73
ALTUM-02 <martin249jesus@gmail.com>
2026-07-22 18:54:06 +0300

    new change

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,19 +4,28 @@ import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
- plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
   server: {
     host: "127.0.0.1",
     port: 5173,
+
     proxy: {
       "/graphql": {
         target: "http://127.0.0.1:8000",
         changeOrigin: true,
       },
+
       "/api": {
         target: "http://127.0.0.1:8000",
         changeOrigin: true,
       },
     },
-  },  
+  },
 });
