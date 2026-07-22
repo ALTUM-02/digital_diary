fae0b6743505fa34143d50da26256f13f5015ee7
ALTUM-02 <martin249jesus@gmail.com>
2026-07-22 19:28:12 +0300

    new change

---
+++ web/src/components/layout/AppLayout.tsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+import { Sidebar } from "./Sidebar";
+import { Header } from "./Header";
+
+export function AppLayout() {
+  return (
+    <div className="flex h-screen overflow-hidden bg-background">
+      {/* Desktop sidebar */}
+      <div className="hidden lg:block">
+        <Sidebar />
+      </div>
+
+      {/* Main */}
+      <div className="flex flex-1 flex-col overflow-hidden">
+        <Header />
+        <main className="flex-1 overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+}
