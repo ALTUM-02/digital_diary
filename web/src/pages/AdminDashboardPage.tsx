510f87bdf84f76a516d2085f4eee5a361ae06bd9
alobit21 <macmtavangu@gmail.com>
2026-07-25 10:30:00 +0300

    Updated project files

--- web/src/pages/AdminDashboardPage.tsx
+++ web/src/pages/AdminDashboardPage.tsx
@@ -4,7 +4,7 @@ import { Card } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
-  BarChartCard,
+  BarChart
   PieChartCard,
   LineChartCard,
   DonutChartCard,
